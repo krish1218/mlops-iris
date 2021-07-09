bebf87cd7a08523c6e365fb5633e92629e0a9914
krishna <krish1218@gmail.com>
2021-07-09 11:05:43 +0530

    modified Readme file and performed assignment tasks

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from datetime import datetime
 
 # test to check the correct functioning of the /ping route
 def test_ping():
@@ -7,7 +8,7 @@ def test_ping():
         response = client.get("/ping")
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"ping": "pong"}
+        assert response.json() == {"ping": "pong","timestamp":datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
 
 
 # test to check if Iris Virginica is classified correctly
@@ -17,10 +18,32 @@ def test_pred_virginica():
         "sepal_length": 3,
         "sepal_width": 5,
         "petal_length": 3.2,
-        "petal_width": 4.4,
+        "petal_width": 4.4
     }
     with TestClient(app) as client:
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json() == {"flower_class": "Iris Virginica","timestamp":datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
+
+#Task2 Writing test cases
+def test_appstatus():
+    with TestClient(app) as client:
+        response = client.get("/appstatus")
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"app": "running successfully","timestamp":datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
+
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.7,
+        "sepal_width": 3.8,
+        "petal_length": 1.7,
+        "petal_width": 0.3
+    }
+    with TestClient(app) as client:
+        response = client.post("/pred_setosa", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa","timestamp":datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
